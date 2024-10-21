8f728d4cdf70a05d6759476654e9b1c33683aac9
khodya <nikitamason@mail.ru>
2024-10-21 15:29:12 +0700

    проверка2

--- pr001.java
+++ pr001.java
@@ -1,6 +1,35 @@
 class pr001 {
 	public static void main(String args[]){
 		System.out.println("Hell");
-	}
+		//преобразование типов 
+		
+		//целые типы данных 
+		byte b; //8 bit
+
+		b = -128;
+		b = 127;
+		b = (byte)130;
+		System.out.println(b);
+		short sh; //16 bit
+		sh = (shor)65536
+		int intg; //32 bit
+		long l; //64 bit
+		intg = 2000000000;
+		intg = intg * intg;
+
+
+		//вещественные типы данных
+		float f; //32 bit
+		double dd; //64 bit
+
+		//символьный тип данных
+		char ch; //16 bit
+		
+		//логический тип
+		boolean bool;
+		bool = true;
+		bool = false;
+		
+
 	
 }
