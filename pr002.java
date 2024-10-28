82b3e1bdea5c34af7a635640142e1129ed7498d2
khodya <nikitamason@mail.ru>
2024-10-28 15:38:34 +0700

    добавили циклы break и continue

--- pr002.java
+++ pr002.java
@@ -82,13 +82,92 @@ class pr002{
 
 		System.out.println("\n");
 		//цикл do while
-		char ch2;
+		/*char ch2;
 		do {
 			System.out.println("введите символ и нажмите Enter: ");
 			ch2 = (char) System.in.read();
-		} while (ch2 != 'q');
+		} while (ch2 != 'q');*/
 		//пример вывхова функции
 		int ewsult = func1();
+		
+		System.out.println();
+		//оператор break
+		for (int i = 0; i < 100; i++){
+			System.out.println("значение i: " + i);
+			if (i == 5)
+				break;
+			System.out.println("следующая итерация");
+		}
+
+		System.out.println();
+		//вложенный цикл в break
+		for ( int i = 0; i < 3; i++){
+			System.out.println("счетчик внешнего цикла:" + i);
+			System.out.print("  счетчик внутреннего цикла: ");
+			int t = 0;
+			while (t < 100){
+				if (t == 10)
+					break;
+				System.out.println(t + " ");
+				t++;
+			}
+			System.out.println();
+		}
+
+		//использование break с меткой
+		System.out.println();
+		int i2;
+		for (i2 = 1; i2 < 4; i2++){
+one:		  {  
+two:			{	
+three:				{
+					System.out.println("\ni2 равно " + i2);
+					if (i2 == 1) break one;
+					if (i2 == 2) break two;
+					if (i2 == 3) break three;
+
+					System.out.println("не выполняется");
+				}
+				System.out.println("после блока three");
+			}
+			System.out.println("после блока two");			
+		  }			
+		  System.out.println("после блока one");
+		}
+		System.out.println("после цикла for");
+		
+
+		//еще один пример break с меткой 
+		System.out.println();
+done:
+		for (int i = 0; i < 10; i++){
+			for (int j = 0; j < 10; j++){
+				for (int k = 0; k < 10; k++){
+					System.out.println(k + " ");
+					if (k == 5) break done;
+				}
+				System.out.println("после цикла k");
+			}
+			System.out.println("псоле цикла j");
+		}
+		System.out.println("после цикла i\n");
+
+
+		//оператор continue
+		for (int i = 0; i <= 100; i++){
+			if ((i % 2) != 0)
+				continue;
+			System.out.println(i);
+		}
+outerloop:
+		for (int i = 1; i < 10; i++){
+			System.out.println("\nпроход внешнего цикла №" + i + ", внетренний цикл: ");
+			for (int j = 1; j < 10; j++){
+				if (j == 5) continue outerloop;
+				System.out.println(j);
+			}
+		}
+		System.out.println();
 	}
 
 
